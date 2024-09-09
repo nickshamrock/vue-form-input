c6eed39fc9ef3d5002b82f182aa86129bb13e18a
Nikolay <nickshamrock1997@gmail.com>
2024-09-09 19:30:44 +0300

    migrate to save data inpit from localstorage to pinia store

--- src/pages/PreviewPage.vue
+++ src/pages/PreviewPage.vue
@@ -1,23 +1,10 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { computed } from 'vue';
+import { useInputStore } from '@/store/InputStore'; // Подключение хранилища Pinia
 
-const userName = ref('Имя');
-const userAge = ref(0);
-const children = ref([]);
+const inputStore = useInputStore();
 
-// Получаем из localStorage данные, которые ввели в HomePage
-onMounted(() => {
-  const savedData = localStorage.getItem('formData');
-
-  if (savedData) {
-    const parsedData = JSON.parse(savedData);
-    userName.value = parsedData.userName;
-    userAge.value = parsedData.userAge;
-    children.value = parsedData.children;
-  }
-});
-
-//Функция, которая меняет "год", "года" и "лет" в зависимости от возраста пользователя или ребенка
+// Функция для склонения "год", "года", "лет"
 const getAgeSuffix = (age) => {
   if (age % 10 === 1 && age % 100 !== 11) {
     return 'год';
@@ -28,18 +15,15 @@ const getAgeSuffix = (age) => {
   }
 };
 
-// условие показа дисклеймера "Вы не указали детей", если нет инфы по детям из массива children, который мы заполняем из localStorage.
-const hasValidChildren = () => {
-  return children.value.some((child) => child.name.trim() !== '' || child.age !== '');
-};
-//условия показа дисклеймера "Вы не указали все данные", если пользователь ввел не все персональные данные
-const hasValidUser = () => {
-  return userName.value.trim() !== '' && userAge.value !== null;
-};
+// Условие показа дисклеймера "Вы не указали детей"
+const hasValidChildren = computed(() => {
+  return inputStore.children.some((child) => child.name.trim() && child.age);
+});
 
-// import { useInputStore } from '@/store/InputStore';
-
-// const inputStore = useInputStore();
+// Условие показа дисклеймера "Вы не указали все данные"
+const hasValidUser = computed(() => {
+  return inputStore.userName.trim() && inputStore.userAge;
+});
 </script>
 
 <template>
@@ -53,16 +37,21 @@ const hasValidUser = () => {
       <div class="flex w-full max-w-[197px] flex-col gap-[60px]">
         <div class="flex flex-col gap-5">
           <h2 class="font-medium">Персональные данные</h2>
-          <p v-if="!hasValidUser()">Вы не указали данные</p>
-          <span v-else class="font-bold">{{ userName }}, {{ userAge }} {{ getAgeSuffix(userAge) }} </span>
-          <!-- <p>{{ inputStore.userName }}</p> -->
+          <p v-if="!hasValidUser">Вы не указали данные</p>
+          <span v-else class="font-bold"
+            >{{ inputStore.userName }}, {{ inputStore.userAge }} {{ getAgeSuffix(inputStore.userAge) }}</span
+          >
         </div>
         <!-- Здесь отображаются данные детей из страницы HomePage -->
         <div>
           <h3 class="mb-5 font-medium">Дети</h3>
-          <p v-if="!hasValidChildren()">Вы не указали детей</p>
+          <p v-if="!hasValidChildren">Вы не указали детей</p>
           <ul v-else class="flex flex-col gap-5">
-            <li v-for="(child, index) in children" :key="index" class="w-fit rounded-[5px] bg-[#F1F1F1] px-5 py-[10px] font-bold">
+            <li
+              v-for="(child, index) in inputStore.children"
+              :key="index"
+              class="w-fit rounded-[5px] bg-[#F1F1F1] px-5 py-[10px] font-bold"
+            >
               {{ child.name.trim() }}, {{ child.age }} {{ getAgeSuffix(child.age) }}
             </li>
           </ul>
--- src/store/InputStore.js
+++ src/store/InputStore.js
@@ -3,12 +3,34 @@ import { defineStore } from 'pinia';
 
 export const useInputStore = defineStore('inputStore', {
   state: () => ({
-    userName: '' // Здесь будем хранить имя пользователя
-  })
-  //   actions: {
-  //     // Экшен для обновления имени пользователя
-  //     updateUserName(newName) {
-  //       this.userName = newName;
-  //     }
-  //   }
+    userName: '',
+    userAge: null,
+    children: []
+  }),
+  actions: {
+    updateUserName(newName) {
+      this.userName = newName;
+    },
+
+    updateUserAge(newAge) {
+      this.userAge = newAge;
+    },
+
+    addChild() {
+      if (this.children.length < 5) {
+        this.children.push({ name: '', age: null });
+      }
+    },
+
+    removeChild(index) {
+      this.children.splice(index, 1);
+    },
+
+    updateChild(index, newName, newAge) {
+      if (index >= 0 && index < this.children.length) {
+        this.children[index].name = newName;
+        this.children[index].age = newAge;
+      }
+    }
+  }
 });
